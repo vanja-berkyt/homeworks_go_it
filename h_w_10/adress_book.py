0c72b5a153650deab4606745d8bfde5c525e4422
vanja-berkyt <i.dragomeretskyy@gmail.com>
2022-08-26 05:22:18 +0300

    field and name added

--- h_w_10/adress_book.py
+++ h_w_10/adress_book.py
@@ -2,11 +2,33 @@ from collections import UserDict
 from typing import Union
 
 
+
+
+
+class Field:
+    def __init__(self, value: str):
+        self.value = value
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    pass
+
+
 class AddressBook(UserDict):
 
-    def add_contact(self, name: str, phone: Union[str, list] = None):
+    def add_contact(self, name: Name, phone: Phone = None):
         contact = Record(name=name, phone=phone)
-        self.data[name] = contact
+        self.data[name.value] = contact
+
+    def add_record(self, record: "Record"):
+        self.data[record.name.value] = record
+
+    def find_contact(self, attribute, value):
+
 
     def change_contact(self, name, old_number, new_number):
         try:
@@ -15,37 +37,30 @@ class AddressBook(UserDict):
         except ValueError:
             return f"{old_number} doesen't exists in book"
 
-
 class Record:
-    def __init__(self, name: str, phone: Union [str, list] = None):
-        self.name = name
-        self.phones = [phone] if phone is not None else []
+    def __init__(self, name: Name, phone: Phone = None):
+        self.name: Name = name
+        self.phones: list[Phone] = [phone] if phone is not None else []
 
-    def add_phone(self, phone_number: str):
+    # def __repr__(self):
+    #     return f"{self.name.value}: {' '.join(phone.value for phone in self.phones)}"
+
+    def add_phone(self, phone_number: Phone):
         self.phones.append(phone_number)
 
-    def change_phone(self, old_number, new_number):
+    def change_phone(self, old_number: Phone, new_number: Phone):
         try:
             self.phones.remove(old_number)
             self.phones.append(new_number)
         except ValueError:
-            return f"{old_number} doesen't exists in book"
+            return f"{old_number} doesn't exists in book"
 
-    def delite_phone(self,phone_number: str):
+    def delite_phone(self, phone_number: Phone):
 
         try:
             self.phones.remove(phone_number)
         except ValueError:
-            return f"{phone_number} doesen't exists in book"
+            return f"{phone_number} doesn't exists in book"
 
 
 
-class Field:
-    def __init__(self, value: str):
-
-
-
-class Name(Field):
-    pass
-class Phone(Field):
-    pass
